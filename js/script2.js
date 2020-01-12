ac0d0e96bd7ef377cba98ffab769ae18d0234e07
djokofloko <57675981+djokofloko@users.noreply.github.com>
2020-01-12 19:17:25 +0100

    Add files via upload

--- js/script2.js
+++ js/script2.js
@@ -110,4 +110,55 @@ $(document).ready(function () {
             // $("#content_div2_2").css("display", "none");
         }
     });
+    
+    $("#nav_el1_2").hover(function (e) {
+        $("#nav_el1_2").css("background-color", "rgba(0, 0, 0, 0.3)")
+    });
+    
+    $("#nav_el2_2").hover(function (e) {
+        $("#nav_el2_2").css("background-color", "rgba(0, 0, 0, 0.3)")
+    });
+    
+    $("#nav_el3_2").hover(function (e) {
+        $("#nav_el3_2").css("background-color", "rgba(0, 0, 0, 0.3)")
+    });
+    
+    $("#nav_el4_2").hover(function (e) {
+        $("#nav_el4_2").css("background-color", "rgba(0, 0, 0, 0.3)")
+    });
+    
+    $("#nav_el5_2").hover(function (e) {
+        $("#nav_el5_2").css("background-color", "rgba(0, 0, 0, 0.3)")
+    });
+    
+    $("#nav_el6_2").hover(function (e) {
+        $("#nav_el6_2").css("background-color", "rgba(0, 0, 0, 0.3)")
+    });
+    
+    
+    $("#nav_el1_2").mouseleave(function (e) {
+        $("#nav_el1_2").css("background-color", "rgba(0, 0, 0, 0.4)")
+    });
+    
+    $("#nav_el2_2").mouseleave(function (e) {
+        $("#nav_el2_2").css("background-color", "rgba(0, 0, 0, 0.4)")
+    });
+    
+    $("#nav_el3_2").mouseleave(function (e) {
+        $("#nav_el3_2").css("background-color", "rgba(0, 0, 0, 0.4)")
+    });
+    
+    $("#nav_el4_2").mouseleave(function (e) {
+        $("#nav_el4_2").css("background-color", "rgba(0, 0, 0, 0.4)")
+    });
+    
+    $("#nav_el5_2").mouseleave(function (e) {
+        $("#nav_el5_2").css("background-color", "rgba(0, 0, 0, 0.4)")
+    });
+    
+    $("#nav_el6_2").mouseleave(function (e) {
+        $("#nav_el6_2").css("background-color", "rgba(0, 0, 0, 0.4)")
+    });
+    
+    
 });
